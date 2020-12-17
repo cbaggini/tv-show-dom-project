7abd022f1b59c8f348da57805195580a51e54cd6
Cecilia <earwen1986@gmail.com>
2020-12-17 14:47:37 +0000

    Completed level 100

--- script.js
+++ script.js
@@ -6,7 +6,16 @@ function setup() {
 
 function makePageForEpisodes(episodeList) {
   const rootElem = document.getElementById("root");
-  rootElem.textContent = `Got ${episodeList.length} episode(s)`;
+  let str = '';
+  episodeList.forEach(function (el) {
+	let episodeCode = `S${String(el.season).padStart(2, '0')}E${String(el.number).padStart(2, '0')}`
+	str += `<section>
+			<div class="title"><h3>${episodeCode} - ${el.name}</h3></div>
+			<img src=${el.image.medium}>
+			<article>${el.summary}</article>
+			</section>`;
+  })
+  rootElem.innerHTML = str;
 }
 
 window.onload = setup;
