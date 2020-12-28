809384f13aecc40a1cc072661696ca2164575195
Cecilia <earwen1986@gmail.com>
2020-12-28 19:58:50 +0000

    Improved speed (removed unnecessary page loads) and added series name in episode view

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-//You can edit ALL of the code here
+// Setup initial view (series list)
 function setup() {
   let seriesList = getAllShows();
   seriesList = seriesList.sort((a,b) => (a.name > b.name) ? 1 : ((b.name > a.name) ? -1 : 0)); 
@@ -26,7 +26,8 @@ function loadSeriesView(seriesList) {
 	const rootElem = document.getElementById("root");
 	rootElem.innerHTML = "";
 	makePageForShows(seriesList);
-	addEpisodeClick(seriesList);
+	addColor();
+	addEpisodeClick();
 	createSeriesSearchBar(seriesList);
 	addSeriesSearchFunction(seriesList);
 }
@@ -41,59 +42,67 @@ function makePageForShows(seriesList) {
   const series = document.createElement("div");
   series.id = "series";
   let str = '';
-  seriesList.forEach(function (el) {
-	let image = el.image ? el.image.medium : "http://via.placeholder.com/210x295/0000FF/808080/?Text=Image%20not%20available";
-	let summary = el.summary ? el.summary : "<p>Summary not available</p>";
-	str += `<section class="seriesClass" id="https://api.tvmaze.com/shows/${el.id}/episodes">
-			<div class="seriesTitle"><h1>${el.name}</h1></div>
+  for (let i=0; i<seriesList.length; i++) {
+	  let image = seriesList[i].image ? seriesList[i].image.medium : "http://via.placeholder.com/210x295/0000FF/808080/?Text=Image%20not%20available";
+	  let summary = seriesList[i].summary ? seriesList[i].summary : "<p>Summary not available</p>";
+	  str += `<section class="seriesClass" id="https://api.tvmaze.com/shows/${seriesList[i].id}/episodes">
+			<div class="seriesTitle"><h1>${seriesList[i].name}</h1></div>
 			<div class="seriesDescription">
 				<img src=${image}>
 				<article class="seriesArticle">${summary}</article>
 				<aside>
-					<p><strong>Rated:&nbsp;</strong>${el.rating.average}</p>
-					<p><strong>Genres:&nbsp;</strong>${el.genres.join(" | ")}</p>
-					<p><strong>Status:&nbsp;</strong>${el.status}</p>
-					<p><strong>Runtime:&nbsp;</strong>${el.runtime}</p>
+					<p><strong>Rated:&nbsp;</strong>${seriesList[i].rating.average}</p>
+					<p><strong>Genres:&nbsp;</strong>${seriesList[i].genres.join(" | ")}</p>
+					<p><strong>Status:&nbsp;</strong>${seriesList[i].status}</p>
+					<p><strong>Runtime:&nbsp;</strong>${seriesList[i].runtime}</p>
 				</aside>
 			</div>
 			</section>`;
-  })
+  }
   series.innerHTML = str;
   rootElem.append(series);
-  document.querySelectorAll("img").forEach(img => {
-	  if (img.src.slice(0,13) === "http://static") {
-		let color = get_average_rgb(img);
-		let c1 = color[0] + 80 <= 255 ? color[0] + 80 : 255;
-		let c2 = color[1] + 80 <= 255 ? color[1] + 80 : 255;
-		let c3 = color[2] + 80 <= 255 ? color[2] + 80 : 255;
-		color = [c1,c2,c3];
-		img.parentElement.parentElement.style.backgroundColor = `rgb(${c1},${c2},${c3})` }
-	  });
+}
+
+// Add background color to series div
+function addColor() {
+	let img = document.querySelectorAll("img");
+	for (let i=0; i<img.length; i++) {
+		if (img[i].src.slice(0,13) === "http://static") {
+			let color = get_average_rgb(img[i]);
+			let c1 = color[0] + 80 <= 255 ? color[0] + 80 : 255;
+			let c2 = color[1] + 80 <= 255 ? color[1] + 80 : 255;
+			let c3 = color[2] + 80 <= 255 ? color[2] + 80 : 255;
+			color = [c1,c2,c3];
+			img[i].parentElement.parentElement.style.backgroundColor = `rgb(${c1},${c2},${c3})`
+		}
+	}
 }
 
 // When user clicks on series, they will go to episode view
-function addEpisodeClick(seriesList) {
-	document.querySelectorAll(".seriesClass").forEach(
-		el => el.addEventListener("click", function() {
-			let series = el.id;
-			let color = el.style.backgroundColor;
-			loadEpisodeView(series, color);
+function addEpisodeClick() {
+	let seriesList = document.querySelectorAll(".seriesClass");
+	for (let i=0; i<seriesList.length; i++) {
+		seriesList[i].addEventListener("click", function() {
+			let series = seriesList[i].id;
+			let color = seriesList[i].style.backgroundColor;
+			let seriesName = seriesList[i].children[0].innerText;
+			loadEpisodeView(series, color, seriesName);
 		})	
-	)
+	}
 }
 
 // Load episode view
-function loadEpisodeView(series, color) {
+function loadEpisodeView(series, color, seriesName) {
 	const seriesView = document.getElementById("series");
 	seriesView.style.display = "none";
 	const seriesSearch = document.getElementById("seriesSearchBar");
 	seriesSearch.style.display = "none";
 	createSearchBar();
 	fetchData(series).then(allEpisodes => {	
-		makePageForEpisodes(allEpisodes, color);
-		addSearchFunction(allEpisodes, color);
+		makePageForEpisodes(allEpisodes, color, seriesName);
+		addSearchFunction();
 		loadFilter(allEpisodes);
-		filterEpisode(allEpisodes, color);
+		filterEpisode();
 	})
 }
 
@@ -144,8 +153,10 @@ function alphabeticSort(seriesList) {
 		document.getElementById("alphabetic").checked = true;
 		document.getElementById("rating").checked = false;
 		seriesList = seriesList.sort((a,b) => (a.name > b.name) ? 1 : ((b.name > a.name) ? -1 : 0)); 
-		makePageForShows(seriesList);
-		addEpisodeClick(seriesList);
+		for (let i=0; i<seriesList.length; i++) {
+			document.getElementById(`https://api.tvmaze.com/shows/${seriesList[i].id}/episodes`).style.order = `${i + 1}`;
+		}
+		loadSeriesFilter(seriesList);
 	})
 }
 
@@ -155,8 +166,10 @@ function ratingSort(seriesList) {
 		document.getElementById("alphabetic").checked = false;
 		document.getElementById("rating").checked = true;
 		seriesList = seriesList.sort((a,b) => (a.rating.average < b.rating.average) ? 1 : ((b.rating.average < a.rating.average) ? -1 : 0)); 
-		makePageForShows(seriesList);
-		addEpisodeClick(seriesList);
+		for (let i=0; i<seriesList.length; i++) {
+			document.getElementById(`https://api.tvmaze.com/shows/${seriesList[i].id}/episodes`).style.order = `${i + 1}`;
+		}
+		loadSeriesFilter(seriesList);
 	})
 }
 
@@ -164,22 +177,35 @@ function ratingSort(seriesList) {
 function addSeriesSearchFunction(seriesList) {
 	document.querySelector("#seriesSearchInput").addEventListener("input", function(event) {
 		let search = event.target.value;
+		let series = document.querySelectorAll(".seriesClass");
 		if (search === "") {
-			makePageForShows(seriesList);
-			addEpisodeClick(seriesList);
+			for (let i=0; i<series.length; i++) {
+				series[i].style.display = "flex";
+			}
 			loadSeriesFilter(seriesList);
 			let selected = document.querySelector("#selectedSeries");
 			selected.innerHTML = `found ${seriesList.length} shows`;
 			alphabeticSort(seriesList);
 			ratingSort(seriesList);
 		} else {
+			for (let i=0; i<series.length; i++) {
+				series[i].style.display = "none";
+			}
 			let newSeriesList = seriesList.filter(function(el) {
 				return (el.name ? el.name.toLowerCase().includes(search.toLowerCase()) : false) || (el.summary ? el.summary.toLowerCase().includes(search.toLowerCase()) : false)
 			})
 			let selected = document.querySelector("#selectedSeries");
 			selected.innerHTML = `found ${newSeriesList.length} shows`;
-			makePageForShows(newSeriesList);
-			addEpisodeClick(newSeriesList);
+			if (document.getElementById("rating").checked === true) {
+				newSeriesList = newSeriesList.sort((a,b) => (a.rating.average < b.rating.average) ? 1 : ((b.rating.average < a.rating.average) ? -1 : 0));
+			} else {
+				newSeriesList = newSeriesList.sort((a,b) => (a.name > b.name) ? 1 : ((b.name > a.name) ? -1 : 0)); 
+			}
+			for (let i=0; i<newSeriesList.length; i++) {
+				let el = document.getElementById(`https://api.tvmaze.com/shows/${newSeriesList[i].id}/episodes`)
+				el.style.order = `${i + 1}`;
+				el.style.display = "flex";
+			}
 			loadSeriesFilter(newSeriesList);
 			alphabeticSort(newSeriesList);
 			ratingSort(newSeriesList);
@@ -193,15 +219,16 @@ function backToSeries() {
 		document.querySelector(".episodes").remove();
 		document.getElementById("searchBar").remove();
 		const seriesView = document.getElementById("series");
-		seriesView.style.display = "block";
+		seriesView.style.display = "flex";
 		const seriesSearch = document.getElementById("seriesSearchBar");
 		seriesSearch.style.display = "flex";
 	})	
 }
 
-async function fetchData(series){
+// Fetch data from api
+async function fetchData(url){
 	let result;
-	await fetch(series)
+	await fetch(url)
 		.then(response => response.json())
 		.then(data => JSON.stringify(data))
 		.then(data => JSON.parse(data))
@@ -212,26 +239,35 @@ async function fetchData(series){
 }
 
 // Search episodes
-function addSearchFunction(allEpisodes, color) {
+function addSearchFunction() {
 	document.querySelector("#searchInput").addEventListener("input", function(event) {
 		let search = event.target.value;
+		let episodes = document.querySelectorAll(".episodeSection");
 		if (search === "") {
-			makePageForEpisodes(allEpisodes,color);
+			for (let i=0; i<episodes.length; i++) {
+				episodes[i].style.display = "block";
+			}
 			let selected = document.querySelector("#selected");
 			selected.innerHTML = "";
 		} else {
-			let newEpisodes = allEpisodes.filter(function(el) {
-				return (el.name ? el.name.toLowerCase().includes(search.toLowerCase()) : false) || (el.summary ? el.summary.toLowerCase().includes(search.toLowerCase()) : false)
+			let newEpisodes = [...episodes].filter(function(el) {
+				return el.innerText.toLowerCase().includes(search.toLowerCase());
 			})
 			let selected = document.querySelector("#selected");
-			selected.innerHTML = `Displaying ${newEpisodes.length}/${allEpisodes.length} episodes`;
-			makePageForEpisodes(newEpisodes, color);
+			selected.innerHTML = `Displaying ${newEpisodes.length}/${episodes.length} episodes`;
+			for (let i=0; i<episodes.length; i++) {
+				if (newEpisodes.includes(episodes[i])) {
+					episodes[i].style.display = "block";
+				} else {
+					episodes[i].style.display = "none";
+				}
+			}
 		}
 	})
 }
 
 // Load episodes
-function makePageForEpisodes(episodeList, color) {
+function makePageForEpisodes(episodeList, color, seriesName) {
   const rootElem = document.getElementById("root");
   let existingEpisodes = document.querySelector(".episodes");
   if (existingEpisodes) {
@@ -239,39 +275,43 @@ function makePageForEpisodes(episodeList, color) {
   }
   const episodes = document.createElement("div");
   episodes.classList = "episodes";
-  let str = '';
-  episodeList.forEach(function (el) {
-	let episodeCode = `S${String(el.season).padStart(2, '0')}E${String(el.number).padStart(2, '0')}`;
-	let image = el.image ? el.image.medium : "http://via.placeholder.com/250x140/0000FF/808080/?Text=Image%20not%20available";
-	let summary = el.summary ? el.summary : "<p>Summary not available</p>";
+  let str = `<h1>${seriesName}</h1>`;
+  for (let i=0; i<episodeList.length; i++) {
+	let episodeCode = `S${String(episodeList[i].season).padStart(2, '0')}E${String(episodeList[i].number).padStart(2, '0')}`;
+	let image = episodeList[i].image ? episodeList[i].image.medium : "http://via.placeholder.com/250x140/0000FF/808080/?Text=Image%20not%20available";
+	let summary = episodeList[i].summary ? episodeList[i].summary : "<p>Summary not available</p>";
 	str += `<section id=${episodeCode} class="episodeSection">
-			<div class="title"><h4>${episodeCode} - ${el.name}</h4></div>
+			<div class="title"><h4>${episodeCode} - ${episodeList[i].name}</h4></div>
 			<img class="episodeImage" src=${image}>
 			<article class="episodeArticle">${summary}</article>
 			</section>`;
-  })
+  }
   episodes.innerHTML = str;
-  //episodes.style.backgroundColor = `rgb($)`
   rootElem.append(episodes);
-  document.querySelectorAll(".episodeSection").forEach(el => el.style.backgroundColor = color);
-  document.querySelectorAll(".title").forEach(el => el.style.backgroundColor = "white");
+  let eps = document.querySelectorAll(".episodeSection");
+  for (let i=0; i<eps.length; i++) {
+	  eps[i].style.backgroundColor = color;
+  }
   document.querySelector("#searchBar").style.backgroundColor = color;
 }
 
 // Filter episodes
-function filterEpisode(allEpisodes, color) {
+function filterEpisode() {
 	document.querySelector("#episodeFilter").addEventListener("change", function(e) {
 		let selectedEpisode = e.currentTarget.value;
-		//makePageForEpisodes(selectedEpisode);
+		let episodes = document.querySelectorAll(".episodeSection");
 		if (selectedEpisode === "allEpisodes") {
-			makePageForEpisodes(allEpisodes, color);
+			for (let i=0; i<episodes.length; i++) {
+				episodes[i].style.display = "block";
+			}
 		} else {
-			makePageForEpisodes(allEpisodes, color);
 			let selected = document.querySelector("#selected");
 			selected.innerHTML = "";
 			document.querySelector("input").value = "";
 			let showEpisode = document.querySelector(`#${selectedEpisode}`);
-			document.querySelectorAll(".episodeSection").forEach(el => el.style.display = "none");
+			for (let i=0; i<episodes.length; i++) {
+				episodes[i].style.display = "none";
+			}
 			showEpisode.style.display = "block";
 		}
 	});
@@ -280,10 +320,10 @@ function filterEpisode(allEpisodes, color) {
 // Create dropdown menu for episodes
 function loadFilter(episodeList) {
 	let str = '<option value="allEpisodes">See all episodes</option>';
-	episodeList.forEach(function(el) {
-		let episodeCode = `S${String(el.season).padStart(2, '0')}E${String(el.number).padStart(2, '0')}`;
-		str += `<option value=${episodeCode}>${episodeCode} - ${el.name}</option>`
-	})
+	for(let i=0; i<episodeList.length; i++) {
+		let episodeCode = `S${String(episodeList[i].season).padStart(2, '0')}E${String(episodeList[i].number).padStart(2, '0')}`;
+		str += `<option value=${episodeCode}>${episodeCode} - ${episodeList[i].name}</option>`
+	}
 	document.querySelector("#episodeFilter").innerHTML = str;
 }
 
@@ -291,18 +331,18 @@ function loadFilter(episodeList) {
 function filterSeries() {
 	document.querySelector("#seriesFilter").addEventListener("change", function(e) {
 		const selectedSeriesLink = e.currentTarget.value;
-		const color = document.getElementById(`${selectedSeriesLink}`).style.backgroundColor;
-		console.log(color);
-		loadEpisodeView(selectedSeriesLink, color); 
+		const color = document.getElementById(`${selectedSeriesLink}`).style.backgroundColor
+		const seriesName = document.getElementById(`${selectedSeriesLink}`).children[0].innerText;
+		loadEpisodeView(selectedSeriesLink, color, seriesName); 
 	});
 }
 
 // Create dropdown menu for series
 function loadSeriesFilter(seriesList) {
 	let str = "";
-	seriesList.forEach(function(el) {
-		str += `<option value= "https://api.tvmaze.com/shows/${el.id}/episodes">${el.name}</option>`
-	})
+	for(let i=0; i<seriesList.length; i++) {
+		str += `<option value= "https://api.tvmaze.com/shows/${seriesList[i].id}/episodes">${seriesList[i].name}</option>`
+	}
 	document.querySelector("#seriesFilter").innerHTML = str;
 }
 
