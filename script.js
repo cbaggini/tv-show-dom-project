7e14ab4396aa2fee95001b8ba4457ffb37400c0b
Cecilia <earwen1986@gmail.com>
2021-01-01 11:06:46 +0000

    Set image crossorigin anonymous to try fixing security error

--- script.js
+++ script.js
@@ -53,10 +53,12 @@ function get_average_rgb(img) {
     if (typeof img == 'string') {
         var src = img;
         img = new Image;
-        img.setAttribute('crossOrigin', ''); 
+		//img.setAttribute('crossOrigin', ''); 
+		img.crossOrigin = "anonymous";
 		img.src = src;
 	}
-	img.setAttribute('crossOrigin', ''); 
+	//img.setAttribute('crossOrigin', ''); 
+	img.crossOrigin = "anonymous";
     context.imageSmoothingEnabled = true;
 	context.drawImage(img, 0, 0, 1, 1);
     return context.getImageData(0, 0, 1, 1).data.slice(0,3);
